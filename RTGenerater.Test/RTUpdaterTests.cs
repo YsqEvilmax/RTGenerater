490cbfbbafd89fe5e8e9954dcef93a768f532be9
YsqEvilmax <ysq.evilmax@qq.com>
2016-10-17 01:14:02 +1300

    Add project files.

---
+++ RTGenerater.Test/RTUpdaterTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RTGenerater;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RTGenerater.Tests
+{
+    [TestClass()]
+    public class RTUpdaterTests
+    {
+        [TestMethod()]
+        public void FetchRemoteTest()
+        {
+            string filePath = System.IO.Path.Combine("./RouteTables", "RemoteIPs.txt");
+            if (File.Exists(filePath)){
+                File.Delete(filePath);
+            }
+
+            RTUpdater rtu = new RTUpdater();
+            rtu.FetchRemote();
+
+            Assert.IsTrue(File.Exists(filePath));
+        }
+    }
+}
